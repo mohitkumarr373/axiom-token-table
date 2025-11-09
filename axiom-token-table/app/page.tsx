93ef471a6437bca3273f0473f02847905baa9c94
Mohit Kumar <146851137+mohitkumarr373@users.noreply.github.com>
2025-11-09 22:46:05 +0530

    Delete axiom-token-table directory

--- axiom-token-table/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { TokenTable } from '../widgets/tokenTable/TokenTable';
-
-export default function HomePage() {
-  return <TokenTable />;
-}
--- axiom-token-table/shared/store/store.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  // If you add Redux/other providers, wrap here; else just return children.
-  return <>{children}</>;
-}
